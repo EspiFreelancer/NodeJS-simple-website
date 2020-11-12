4fbff1c2a8e635e16e457f0c4e50a3a3b4ee9ca8
EspiFreelancer <espifreelancer@gmail.com>
2020-11-11 23:54:55 -0300

    Add basic website structure

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,31 @@ router.get('/', async function(req, res, next) {
   res.render('index.njk', data);
 });
 
+/* About */
+router.get('/nosotros', function(req, res, next) {
+	let data = {
+		layout: 'layout.njk',
+		title: 'Sobre nosotros'
+	}
+	res.render('about.njk', data);
+})
+
+/* Services */
+router.get('/servicios', function(req, res, next) {
+	let data = {
+		layout: 'layout.njk',
+		title: 'Servicios'
+	}
+	res.render('services.njk', data);
+})
+
+/* Contact */
+router.get('/contacto', function(req, res, next) {
+	let data = {
+		layout: 'layout.njk',
+		title: 'Contacto'
+	}
+	res.render('contact.njk', data);
+})
+
 module.exports = router;
